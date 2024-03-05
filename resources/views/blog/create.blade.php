7d0de37e6eef1deeff5da594facfc2b3d19c1b42
JdcStig <duffycreagh2003@gmail.com>
2024-03-05 12:18:14 +0000

    Added new fields to the add blog such as the dropdown and the checklist

--- resources/views/blog/create.blade.php
+++ resources/views/blog/create.blade.php
@@ -32,15 +32,59 @@
             type="text"
             name="title"
             placeholder="Title..."
-            class="bg-transparent block border-b-2 w-full h-20 text-6xl outline-none">
+            class="bg-transparent block border-b-2 w-full h-20 text-6xl outline-none black">
+
+
+            <div class = dropdown>
+            <label for="Topic" class="text-choosetopic">Choose a Topic:</label>
+            <select name="genre" id="genre" class="bg-transparent block border-b-2 w-full h-20 text-xl outline-none">
+                <option value="action">Fallout 1</option>
+                <option value="comedy">Fallout 2</option>
+                <option value="drama">Fallout 3</option>
+                <option value="drama">Fallout New Vegas</option>
+                <option value="drama">Fallout 4</option>
+                <option value="drama">Fallout 76</option>
+                <option value="drama">Fallout tv series</option>
+                <option value="drama">Fallout news</option>
+            </select>
+</div>
+
+
+
+<div class="check-list">
+    <label class="text-choosetopic">Select Categories:</label>
+    <div>
+
+        <input type="checkbox" name="categories[]" value="item" class="mr-2">
+        <label for="item" class="mr-4">Item</label>
+
+        <input type="checkbox" name="categories[]" value="random_encounter" class="mr-2">
+        <label for="random_encounter" class="mr-4">Random Encounter</label>
+
+        <input type="checkbox" name="categories[]" value="lore" class="mr-2">
+        <label for="lore" class="mr-4">Lore</label>
+
+        <input type="checkbox" name="categories[]" value="lore" class="mr-2">
+        <label for="lore" class="mr-4">Companion</label>
+
+        <input type="checkbox" name="categories[]" value="lore" class="mr-2">
+        <label for="lore" class="mr-4">Enemy</label>
+        
+        <input type="checkbox" name="categories[]" value="lore" class="mr-2">
+        <label for="lore" class="mr-4">Other</label>
+
+    </div>
+</div>
+
 
         <textarea 
             name="description"
             placeholder="Description..."
             class="py-20 bg-transparent block border-b-2 w-full h-60 text-xl outline-none"></textarea>
 
+            <div class="media">
         <div class="bg-grey-lighter pt-15">
-            <label class="w-44 flex flex-col items-center px-2 py-3 bg-white-rounded-lg shadow-lg tracking-wide uppercase border border-blue cursor-pointer">
+            <label class="w-44 flex flex-col items-center px-2 py-3 bg-white rounded-lg shadow-lg tracking-wide uppercase border border-blue cursor-pointer">
                 <span class="mt-2 text-base leading-normal">
                     Select a file
                 </span>
@@ -50,6 +94,23 @@
                     class="hidden">
             </label>
         </div>
+
+
+        <div class="bg-grey-lighter pt-15">
+            <label class="w-44 flex flex-col items-center px-2 py-3 bg-white rounded-lg shadow-lg tracking-wide uppercase border border-blue cursor-pointer">
+                <span class="mt-2 text-base leading-normal">
+                    Select a Video
+                </span>
+                <input 
+                    type="file"
+                    name="video"
+                    accept="video/*"
+                    class="hidden">
+            </label>
+        </div>
+
+            </div>
+
 
         <button    
             type="submit"
