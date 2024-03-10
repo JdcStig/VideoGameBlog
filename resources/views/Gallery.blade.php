7befc4a0f0cf8860f00068d118b2aa8cfa126da7
JdcStig <duffycreagh2003@gmail.com>
2024-03-10 15:46:48 +0000

    added images to image gallery

--- resources/views/Gallery.blade.php
+++ resources/views/Gallery.blade.php
@@ -14,6 +14,7 @@
           <img src="https://images.ctfassets.net/rporu91m20dc/2HrVpuM90kOCcWYYuw8ss6/cf727be73b5f663b6c5b5ea632f9bbf5/Fallout76_E3_Relic.png?w=502&h=280&fit=thumb&q=70&&&fm=webp" style="width:100%">
           <img src="https://images.ctfassets.net/rporu91m20dc/7nwXfwkSZOg8WkwwcyKsSS/e8e135f62752bb8e584a63ec2dc39e70/Fallout76_E3_T51b.png?w=502&h=280&fit=thumb&q=70&&&fm=webp" style="width:100%">
           <img src="https://images.ctfassets.net/rporu91m20dc/3SOLQRlAStcTP0q7jBzJVN/7f6a6d0821eb40222e25e07b26826bb1/Fallout76-Wastelanders_03_1920x1080-WM.jpg?w=502&h=280&fit=thumb&q=70&&&fm=webp" style="width:100%">
+          <img src="https://images.ctfassets.net/rporu91m20dc/53PwR4dcu7FrRgA1fvyw5Z/b6be94e80461239b5b344402fae1479a/Ranger_Armor.jpg?w=502&h=280&fit=thumb&q=70&&&fm=webp" style="width:100%">
         </div>
 
         <div class="gallerycolumn">
@@ -24,6 +25,7 @@
           <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQWKFojCA_9aXxpzTjojKp7zoROwb7lzJnCOg&usqp=CAU" style="width:100%">
           <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQPAwjxcC9xHRc9mMYGNLQytdpKsCO9wcqviw&usqp=CAU" style="width:100%">
           <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWpD4lbiiwQ_o-P13z0trFoaVdqia5VGvUdg&usqp=CAU" style="width:100%">
+          <img src="https://images.ctfassets.net/rporu91m20dc/6O0i4pI0NQzWGxsFY42yes/0554db3e6c4b14b0a78fc1097a1d6aa4/Fallout_76_Fasnacht_Parade.png?w=502&h=280&fit=thumb&q=70&&&fm=webp" style="width:100%">
         
         </div>  
         <div class="gallerycolumn">
@@ -31,13 +33,11 @@
           <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSG0UaRMxn7Bp0QMKn2WaNmV_m8tHkHCbSDFg&usqp=CAU" style="width:100%">
           <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRjFIt6zOW0gz7GLUP2nkcyCAOyDnoonfZt_28nmji1bd7ujPpF-hhUuiboS3s0-Ww1yzE&usqp=CAU" style="width:100%">
           <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTjEBz2nSz9lvlfFXfc46MgfistkpQBEX36HQ&usqp=CAU" style="width:100%">
-         
-    
           <img src="https://images.ctfassets.net/rporu91m20dc/1BlFvx4C3Y3poV0rLVLLAO/0ff59639398260e1eeaa6ef01fb4fdee/Fallout_76_Wastelanders_Anchor_Farms_WM.png?w=502&h=280&fit=thumb&q=70&&&fm=webp" style="width:100%">
           <img src="https://images.ctfassets.net/rporu91m20dc/19QZ2p3D9zn5sCknaWtGor/32a7b9c8c437abfaf12222157dd54abe/Fallout_76_Ever_Upwards.png?w=502&h=280&fit=thumb&q=70&&&fm=webp" style="width:100%">
           <img src="https://images.ctfassets.net/rporu91m20dc/2PqDuovOudzOp40oMCOWw2/07965290e1111472cc7b000bbf110654/Fallout_76_Welcome_To_Wild_Appalachia.png?w=502&h=280&fit=thumb&q=70&&&fm=webp" style="width:100%">
           <img src="https://images.ctfassets.net/rporu91m20dc/2aTpnMbRscy60gem6kI6ci/06506e98a8b5ac905aa99e7805f853eb/Fallout76_E3_PowerArmors.png?w=502&h=280&fit=thumb&q=70&&&fm=webp" style="width:100%">
-         
+          <img src="https://images.ctfassets.net/rporu91m20dc/4N6335HvTq4ucAWioA80ao/c6aac90a72d3dc2b0e7cd0fbc5522d69/Fallout76_E3_Pip-Boy.png?w=502&h=280&fit=thumb&q=70&&&fm=webp" style="width:100%">
 
 
           </div>
